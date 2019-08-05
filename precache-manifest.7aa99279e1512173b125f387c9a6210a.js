c9cbcedad2de305df5e0af0f5c5f286e4721fd2f
misha <misha.baturin96@gmail.com>
2019-08-05 10:10:39 +0300

    deploy tic tac toe

---
+++ precache-manifest.7aa99279e1512173b125f387c9a6210a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee22121a0aacb0b595ecd89d472693fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de275773845921438754",
+    "url": "/static/css/main.9c58271c.chunk.css"
+  },
+  {
+    "revision": "f679fd4182c5b012196f",
+    "url": "/static/js/2.0949c122.chunk.js"
+  },
+  {
+    "revision": "de275773845921438754",
+    "url": "/static/js/main.17eeed97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
